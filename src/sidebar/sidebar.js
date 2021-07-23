89a3591673bc25c5553604fa15e03ff054ed08ea
li2he1 <peterli2he1@gmail.com>
2021-07-23 12:15:06 -0400

    formate code

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -27,60 +27,60 @@ class SidebarComponent extends React.Component {
     this.setState({ title: null, addingNote: false });
   }
 
-  selectNote = (n, i) => this.props.selectNote(n, i);;
+  selectNote = (n, i) => this.props.selectNote(n, i);
   deleteNote = (note) => this.props.deleteNote(note);
 
   render() {
     const { notes, classes, selectedNoteIndex } = this.props;
-    if(notes){
-    return(
-        <div  className={classes.sidebarContainer}>
-          <Button 
+    if (notes) {
+      return (
+        <div className={classes.sidebarContainer}>
+          <Button
             onClick={this.newNoteBtnClick}
             className={classes.newNoteBtn}>
-              {this.state.addingNote ? 'Cancel' : 'New Note'}
+            {this.state.addingNote ? 'Cancel' : 'New Note'}
           </Button>
           {
-              this.state.addingNote ? 
+            this.state.addingNote ?
               <div>
                 <input type='text'
                   className={classes.newNoteInput}
                   placeholder='Enter note title'
                   onKeyUp={(e) => this.updateTitle(e.target.value)}>
                 </input>
-                <Button 
+                <Button
                   className={classes.newNoteSubmitBtn}
                   onClick={this.newNote}>Submit Note</Button>
               </div> :
               null
+          }
+          <List>
+            {
+              notes.map((_note, _index) => {
+                return (
+                  <div key={_index}>
+                    <SidebarItemComponent
+                      _note={_note}
+                      _index={_index}
+                      selectedNoteIndex={selectedNoteIndex}
+                      selectNote={this.selectNote}
+                      deleteNote={this.deleteNote}>
+                    </SidebarItemComponent>
+                    <Divider></Divider>
+                  </div>
+                )
+              })
             }
-            <List>
-              {
-                notes.map((_note, _index) => {
-                  return(
-                    <div key={_index}>
-                      <SidebarItemComponent
-                        _note={_note}
-                        _index={_index}
-                        selectedNoteIndex={selectedNoteIndex}
-                        selectNote={this.selectNote}
-                        deleteNote={this.deleteNote}>
-                      </SidebarItemComponent>
-                      <Divider></Divider>
-                    </div>
-                  )
-                })
-              }
-            </List>
+          </List>
 
         </div>
       );
     } else {
-      return(<div></div>);
+      return (<div></div>);
     }
   }
 
-  
+
 
 }
 
