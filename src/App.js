649ab05e88f01640d8caa7fd51a67a89ea469c4c
li2he1 <peterli2he1@gmail.com>
2021-07-23 11:46:59 -0400

    new note

--- src/App.js
+++ src/App.js
@@ -42,6 +42,25 @@ export default class App extends React.Component {
       });
   }
 
+  newNote = async (title) => {
+    const note = {
+      title: title,
+      body: ''
+    };
+    const newFromDB = await firebase
+      .firestore()
+      .collection('notes')
+      .add({
+        title: note.title,
+        body: note.body,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      });
+    const newID = newFromDB.id;
+    await this.setState({ notes: [...this.state.notes, note] });
+    const newNoteIndex = this.state.notes.indexOf(this.state.notes.filter(_note => _note.id === newID)[0]);
+    this.setState({ selectedNote: this.state.notes[newNoteIndex], selectedNoteIndex: newNoteIndex });
+  }
+
 
   render(){
     return (
